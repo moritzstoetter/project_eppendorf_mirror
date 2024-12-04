bbc953813c4c021e0eccc23c79dee72ea898ca8b
Moritz Stoetter <hi@moritzstoetter.dev>
2024-12-04 08:45:59 +0100

    formatting

--- app/app_main.cpp
+++ app/app_main.cpp
@@ -1,5 +1,5 @@
 #include "esp_log.h"
 
 extern "C" void app_main(void) {
-    ESP_LOGI("app_main", "Ahoi Discoverer, your quest begins here!");
+  ESP_LOGI("app_main", "Ahoi Discoverer, your quest begins here!");
 }
--- app/edf_stubs.cpp
+++ app/edf_stubs.cpp
@@ -8,43 +8,60 @@
 
 namespace edf::hw::mcu {
 
-auto get_isr_number() noexcept -> uint32_t {
-    // Unimplemented.
-    return 0;
+uint32_t get_isr_number() noexcept {
+  // Unimplemented.
+  return 0;
 }
 
-auto reset() noexcept -> void {
-    esp_restart();
+void reset() noexcept {
+  esp_restart();
 }
 
-auto get_hardware_reset_reason() noexcept -> std::optional<std::string_view> {
-    switch (esp_reset_reason()) {
-    case ESP_RST_UNKNOWN:    return {"ESP_RST_UNKNOWN"};
-    case ESP_RST_POWERON:    return {"ESP_RST_POWERON"};
-    case ESP_RST_EXT:        return {"ESP_RST_EXT"};
-    case ESP_RST_SW:         return {"ESP_RST_SW"};
-    case ESP_RST_PANIC:      return {"ESP_RST_PANIC"};
-    case ESP_RST_INT_WDT:    return {"ESP_RST_INT_WDT"};
-    case ESP_RST_TASK_WDT:   return {"ESP_RST_TASK_WDT"};
-    case ESP_RST_WDT:        return {"ESP_RST_WDT"};
-    case ESP_RST_DEEPSLEEP:  return {"ESP_RST_DEEPSLEEP"};
-    case ESP_RST_BROWNOUT:   return {"ESP_RST_BROWNOUT"};
-    case ESP_RST_SDIO:       return {"ESP_RST_SDIO"};
-    case ESP_RST_USB:        return {"ESP_RST_USB"};
-    case ESP_RST_JTAG:       return {"ESP_RST_JTAG"};
-    case ESP_RST_EFUSE:      return {"ESP_RST_EFUSE"};
-    case ESP_RST_PWR_GLITCH: return {"ESP_RST_PWR_GLITCH"};
-    case ESP_RST_CPU_LOCKUP: return {"ESP_RST_CPU_LOCKUP"};
-    default:                 return std::nullopt;
-    }
+std::optional<std::string_view> get_hardware_reset_reason() noexcept {
+  switch (esp_reset_reason()) {
+  case ESP_RST_UNKNOWN:
+    return {"ESP_RST_UNKNOWN"};
+  case ESP_RST_POWERON:
+    return {"ESP_RST_POWERON"};
+  case ESP_RST_EXT:
+    return {"ESP_RST_EXT"};
+  case ESP_RST_SW:
+    return {"ESP_RST_SW"};
+  case ESP_RST_PANIC:
+    return {"ESP_RST_PANIC"};
+  case ESP_RST_INT_WDT:
+    return {"ESP_RST_INT_WDT"};
+  case ESP_RST_TASK_WDT:
+    return {"ESP_RST_TASK_WDT"};
+  case ESP_RST_WDT:
+    return {"ESP_RST_WDT"};
+  case ESP_RST_DEEPSLEEP:
+    return {"ESP_RST_DEEPSLEEP"};
+  case ESP_RST_BROWNOUT:
+    return {"ESP_RST_BROWNOUT"};
+  case ESP_RST_SDIO:
+    return {"ESP_RST_SDIO"};
+  case ESP_RST_USB:
+    return {"ESP_RST_USB"};
+  case ESP_RST_JTAG:
+    return {"ESP_RST_JTAG"};
+  case ESP_RST_EFUSE:
+    return {"ESP_RST_EFUSE"};
+  case ESP_RST_PWR_GLITCH:
+    return {"ESP_RST_PWR_GLITCH"};
+  case ESP_RST_CPU_LOCKUP:
+    return {"ESP_RST_CPU_LOCKUP"};
+  default:
+    return std::nullopt;
+  }
 }
 
-} // namespace edf::hw::mcu
+}    // namespace edf::hw::mcu
 
 namespace edf::fail {
 
-auto get_failure_handler() -> failure_handler & {
-    std::terminate();
+failure_handler& get_failure_handler() {
+  std::terminate();
 }
 
-} // namespace edf::fail
+}    // namespace edf::fail
